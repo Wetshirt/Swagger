652fd807351d5854d9937a935f3aec39fa2bbe83
wetshirt <sam9310505@gmail.com>
2020-04-22 09:49:55 +0800

    no

---
+++ src/main/java/com/example/demo/SwaggerConfig.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.util.Collections;
+
+/**
+ * @author ganchaoyang
+ * @date 2019/3/1014:16
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	 @Bean
+	    public Docket createRestApi() {
+	        return new Docket(DocumentationType.SWAGGER_2)
+	                .apiInfo(apiInfo())
+	                .select()
+	                .apis(RequestHandlerSelectors.basePackage("com.example.demo.controller"))
+	                .paths(PathSelectors.any())
+	                .build();
+	    }
+
+	    private ApiInfo apiInfo() {
+	        return new ApiInfoBuilder()
+	                .title("我是標題")
+	                .description("我是敘述")
+	                
+	          
+	                .version("1.0")
+	                .build();
+	    }
+	
+	
+}
